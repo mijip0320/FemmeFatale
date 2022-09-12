44e35f57dda9249ca1626251a5447263dc21494b
jimin <39897338+jiminlim@users.noreply.github.com>
2022-09-12 21:47:27 +0900

    Create pro_타겟넘버_jiminlim.java

---
+++ 8월5주차/pro_타겟넘버_jiminlim.java
@@ -0,0 +1,25 @@
+	public static void main(String[] args) {
+		System.out.println(solution(new int[] {4, 1, 2, 1}, 4));
+
+	}
+	
+	private static int count ;
+	public static int solution(int[] numbers, int target) {
+		count =0;
+		
+		back(numbers, 0,0,target); 
+		
+		return count;
+	}
+	private static void back(int[] numbers, int idx, int sum, int target) {
+		if(numbers.length == idx) {
+			if(target == sum) {
+				count ++;
+			}			
+			return;
+		}
+		//더하기 먼저 시작
+		back(numbers, idx+1, sum+numbers[idx], target);
+		//뺄셈 먼저 시작
+		back(numbers, idx+1, sum-numbers[idx], target);
+	}
