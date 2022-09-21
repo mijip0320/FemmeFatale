eacf1931d73ee330ac375624cdbbc66499657a8a
jimin <39897338+jiminlim@users.noreply.github.com>
2022-09-22 07:05:28 +0900

    20220922

---
+++ 9월1주차/pro_행렬의곱셈_jiminlim.java
@@ -0,0 +1,16 @@
+ public static void main(String[] args) {
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3,3},{3,3}};
+
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for(int i=0; i< arr1.length; i++){
+            for(int j=0; j<arr2[0].length; j++){
+                int m=0;
+                while(m < arr1[0].length){
+                    answer[i][j] += arr1[i][m] * arr2[m][j];
+                    m++;
+                }
+            }
+
+        }
