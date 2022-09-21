6bcebcd383d5fe25cd6c2088813e4e62a70ecb25
jimin <39897338+jiminlim@users.noreply.github.com>
2022-09-22 07:06:34 +0900

    20220922

---
+++ pro_예상대진표_jiminlim.java
@@ -0,0 +1,25 @@
+public int solution(int n, int a, int b){
+        int answer = 1;
+        int left = 0;
+        int right = 0;
+        
+        if(a > b){
+            left = b;
+            right = a;
+        } else {
+            left = a;
+            right = b;
+        }
+        
+        while(true){
+            if(left % 2 != 0 && right - left == 1){
+                break;
+            }
+            
+            left = (left + 1) / 2;
+            right = (right + 1) / 2;
+            answer++;
+        }
+
+        return answer;
+    }
