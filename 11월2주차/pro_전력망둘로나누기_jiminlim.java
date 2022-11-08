fba1b29f51e0977dd0a9f64a731d4d31c50d1adc
jimin <39897338+jiminlim@users.noreply.github.com>
2022-11-08 22:11:04 +0900

    20221108_dfs

--- 11월2주차/pro_전력망둘로나누기_jiminlim.java
+++ 11월2주차/pro_전력망둘로나누기_jiminlim.java
@@ -5,30 +5,13 @@ class Solution {
     public static int n, a,b;
     public static boolean[] visit;
     public int solution(int n, int[][] wires) {
-        ArrayList<int[]> wire = new ArrayList<>();
-         //hash = new HashMap<>();
-        n = 0;
-        for(int i =0; i< wires.length; i++){
-            wire.add(new int[]{wires[i][0], wires[i][1]});
-            n = Math.max(n, Math.max(wires[i][0],wires[i][1]));
-        }
+        n=n;
         map = new int[n+1][n+1];
-        //System.out.println("ddddd "+n);
-        for(int i = 0;i< wire.size(); i++){
-            int[] arr = wire.get(i);
-            map[arr[0]][arr[1]] = 1;
-            map[arr[1]][arr[0]] = 1;
+        for(int i = 0;i< wires.length; i++){
+            map[wires[i][0]][wires[i][1]] = 1;
+            map[wires[i][1]][wires[i][0]] = 1;
         }
 
-        // for(int i = 0;i< n+1; i++){
-        //     System.out.println(Arrays.toString(map[i]));
-        // }
-
-        // int index = 0;
-        // for(Integer key : hash.keySet()){            
-        //     hash.put(key,index);
-        //     index ++;
-        // }
 
         int answer = Integer.MAX_VALUE;
         for(int i = 1;i<=n; i++){
@@ -36,36 +19,26 @@ class Solution {
                 if(map[i][j] == 0) continue;
                 visit = new boolean[n+1];
                 visit[i] = true;
-                a=1; b=1;
-               // System.out.println("first"+ i);
+                a=1; 
                 dfs(i,j,1,n);
-                //System.out.println("second" + j);
-                visit[j] = true;
-                dfs(j,i,0,n);
-                //System.out.println(a+" "+b);
-                answer = Math.min(Math.abs(a-b), answer);
+                answer = Math.min(Math.abs(a-(n-a)), answer);
             }
         }
 
 
         return answer;
     }
-    public static void dfs(int num,int cut, int flag, int len){
-        //System.out.println("num : "+num+ "    "+n+"  "+len);        
+    public static void dfs(int num,int cut, int flag, int len){       
 
         for(int i =1; i<= len; i++){
             if(cut == i) continue;
-           // System.out.println("> "+i);
             if(map[num][i] == 0) continue;  
-            //System.out.println(">>>>> "+i);
             if(visit[i]) continue;            
             visit[i] = true;
             if(flag == 1){
                 a ++;   
             } 
-            if(flag == 0) {
-                b++;
-            }
+            
             dfs(i,cut,flag,len);
 
         }
